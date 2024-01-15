2225786a53f1d7dd3d991dbef8fa7635b618c831
sivapriya <sivapriyavimalkumar8@gmail.com>
2024-01-15 15:54:42 +0530

    New update

--- HRM/src/HRM1/CONTACT.java
+++ HRM/src/HRM1/CONTACT.java
@@ -1,5 +1,61 @@
 package HRM1;
 
-public class CONTACT {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+
+public class CONTACT
+{
+	public static WebDriver driver=null;
+	public static ExtentReports reports;
+	public static ExtentTest test;
+
+@BeforeTest
+
+	public void initialize() throws Exception  {
+		//System.setProperty("webdriver.chrome.driver","C:\\Users\\Jimmy Joy\\Desktop\\shabana\\seleniumnew\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+        //driver = new FirefoxDriver();
+	
+		System.out.println("driver initialised");
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\HP\\Downloads\\geckodriver-v0.29.1-win64\\geckodriver.exe");
+		driver=new FirefoxDriver();
+		
+	
+	
+	}
+
+@BeforeSuite
+public void reportGenerate()
+{
+	
+	reports = new ExtentReports(System.getProperty("user.dir")+"ExtentReportResults.html");
+	test = reports.startTest("ExtentDemo");
+	
+}
+
+@AfterSuite
+public void endTest()
+{
+reports.endTest(test);
+reports.flush();
+}
+
+@AfterTest
+public void tearDown() throws WebDriverException
+{
+		
+			
+			//driver.close();
+//			driver.quit();
+
+}
+
 
 }
